d15be9ee83d827a757768436d46f2c2f1fd14177
broxdeez <broxdeez@gmail.com>
2024-04-12 15:18:10 -0700

    added colorscheme

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -7,4 +7,50 @@ lvim.plugins = {
       require("todo-comments").setup()
     end
   },
+  -- Gruvbox
+{ "morhetz/gruvbox" },
+{ "sainnhe/gruvbox-material" },
+-- Sonokai
+{ "sainnhe/sonokai" },
+-- Edge
+{ "sainnhe/edge" },
+-- Horizon
+{ "lunarvim/horizon.nvim" },
+-- Molokai
+{ "tomasr/molokai" },
+-- Ayu
+{ "ayu-theme/ayu-vim" },
+
+-- Onedark
+{ "navarasu/onedark.nvim" },
+
+-- Catppuccin
+{ "catppuccin/nvim", name = "catppuccin" },
+
+-- Everforest
+{ "sainnhe/everforest" },
+
+-- Nord
+{ "shaunsingh/nord.nvim" },
+
+-- Dracula
+{ "Mofiqul/dracula.nvim" },
+
+-- Tokyo Night
+{ "folke/tokyonight.nvim" },
+
+-- Wal
+{ "Mofiqul/vscode.nvim" },
+
+-- Nightfox
+{ "EdenEast/nightfox.nvim" },
+
+-- Palenight
+{ "npxbr/gruvbox.nvim" },
+
+-- Rose Pine
+{ "rose-pine/neovim", name = "rose-pine" },
+
+-- Kanagawa
+{ "rebelot/kanagawa.nvim" },
 }
