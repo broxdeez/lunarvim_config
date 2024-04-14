4a8f133bdebd02cd6552ef8a9bef424731c9df84
broxdeez <broxdeez@gmail.com>
2024-04-13 23:58:01 -0700

    added linter, formatter for python

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -8,3 +8,7 @@ lvim.builtin.treesitter.ensure_installed = {
   "yaml",
   "lua",
 }
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup { { name = "black" } }
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup { { command = "flake8", args = { "--ignore", "E203" }, filetypes = { "python" } } }
